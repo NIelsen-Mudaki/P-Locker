275418cf0d00944d8bb61ed9ff65385db72b08da
Nielsen-Mudaki <jumbanielsen@gmail.com>
2022-02-04 00:27:21 +0300

    add user class

---
+++ pass_locker.py
@@ -0,0 +1,23 @@
+class User:
+    '''
+        Class to generate new instance of a user.
+    '''
+    users_list = []
+
+    def __init__(self,f_name,l_name,password):
+        '''
+        __init__ method that helps to define properties of an object
+        '''
+        self.f_name = f_name
+        self.l_name = l_name
+        self.password = password
+
+    pass
+
+
+class Credentials:
+    '''
+        Class to create new objects of credentials.
+    '''
+
+    pass
