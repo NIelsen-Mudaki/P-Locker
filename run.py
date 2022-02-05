30fdc641a49c7ffc075c1aa4a8a6c0aa81bcc3b5
Nielsen-Mudaki <jumbanielsen@gmail.com>
2022-02-06 02:33:03 +0300

    add functions to run.py file

--- run.py
+++ run.py
@@ -8,3 +8,28 @@ def new_user(username,password):
     '''
     new_user = User(username,password)
     return new_user
+
+def save_user(user):
+    '''
+    Function to save a new user
+    '''
+    user.save_user()
+
+def display_user():
+    """
+    Function to display existing user
+    """
+    return User.display_user()
+
+def create_new_credential(account,user_name,email,password):
+    """
+    Function that creates new credentials for a user
+    """
+    new_credential = Credentials(account,user_name,email,password)
+    return new_credential
+
+def save_credentials(credentials):
+    """
+    Function to save credentials to the credentials list
+    """
+    credentials.save_credentials()
